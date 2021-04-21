71e3551cd3b6a88ab957ad836cfe050479061dbe
Anton Kesy <anton@kesy.de>
2021-04-21 19:52:13 +0200

    Update client.c
    Formatted to C style. Small improvements: Removed unreachable and redundant code.

--- client.c
+++ client.c
@@ -5,21 +5,23 @@
 #include <arpa/inet.h>
 #define SIZE 1024
 
-void send_file_data(FILE *fp, int sockfd, struct sockaddr_in addr){
+void send_file_data(FILE* fp, int sockfd, struct sockaddr_in addr)
+{
   int n;
   char buffer[SIZE];
 
   // Sending the data
-  while(fgets(buffer, SIZE, fp) != NULL){
+  while (fgets(buffer, SIZE, fp) != NULL)
+  {
     printf("[SENDING] Data: %s", buffer);
 
     n = sendto(sockfd, buffer, SIZE, 0, (struct sockaddr*)&addr, sizeof(addr));
-    if (n == -1){
+    if (n == -1)
+    {
       perror("[ERROR] sending data to the server.");
       exit(1);
     }
     bzero(buffer, SIZE);
-
   }
 
   // Sending the 'END'
@@ -27,24 +29,25 @@ void send_file_data(FILE *fp, int sockfd, struct sockaddr_in addr){
   sendto(sockfd, buffer, SIZE, 0, (struct sockaddr*)&addr, sizeof(addr));
 
   fclose(fp);
-  return;
 }
 
-int main(){
+int main(void)
+{
 
   // Defining the IP and Port
   char *ip = "127.0.0.1";
-  int port = 8080;
+  const int port = 8080;
 
   // Defining variables
   int server_sockfd;
   struct sockaddr_in server_addr;
-  FILE *fp;
   char *filename = "client.txt";
+  FILE *fp = fopen(filename, "r");
 
   // Creating a UDP socket
   server_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-  if (server_sockfd < 0){
+  if (server_sockfd < 0)
+  {
     perror("[ERROR] socket error");
     exit(1);
   }
@@ -53,8 +56,8 @@ int main(){
   server_addr.sin_addr.s_addr = inet_addr(ip);
 
   // Reading the text file
-  fp = fopen(filename, "r");
-  if (fp == NULL){
+  if (fp == NULL)
+  {
     perror("[ERROR] reading the file");
     exit(1);
   }
@@ -68,5 +71,4 @@ int main(){
   close(server_sockfd);
 
   return 0;
-
 }
